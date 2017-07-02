a3cbe1519405285b9b3e69490953afd7fe30948f
mengqi <limengqi520@outlook.com>
2017-07-02 09:11:56 +0800

    Initial Commit

---
+++ LMQRouter/LMQRouter.h
@@ -0,0 +1,19 @@
+//
+//  LMQRouter.h
+//  LMQRouter
+//
+//  Created by lizhengheng on 2017/7/2.
+//  Copyright © 2017 limengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LMQRouter.
+FOUNDATION_EXPORT double LMQRouterVersionNumber;
+
+//! Project version string for LMQRouter.
+FOUNDATION_EXPORT const unsigned char LMQRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LMQRouter/PublicHeader.h>
+
+
