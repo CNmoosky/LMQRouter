7722bb48536a8681c1ce1fc9a3157605b177200c
mengqi <limengqi520@outlook.com>
2017-07-08 12:29:33 +0800

    添加注释

--- LMQRouter/LMQRouter.h
+++ LMQRouter/LMQRouter.h
@@ -14,18 +14,60 @@ typedef id(^LMQRouterObjectBlock)(NSDictionary *result);
 
 @interface LMQRouter : NSObject
 
+/**
+ *  注册URLPattern对应的block，在block中可以做各种操作
+ *
+ *  @param URLPattern 例子：lmq://path/:userId
+ *  @param block    该 block 会传一个字典，包含了注册的 URL 中对应的变量。如果注册的URL是lmq://path/:userId 那么，就会传一个 @{@"userId": 4}
+ */
 + (void)registerURLPattern:(NSString *)URLPattern forBlock:(LMQRouterBlock)block;
 
+/**
+ 注册URLPattern对应的可返回值的block，在block中可以做各种操作
+ 
+ @param URLPattern 例子：lmq://path/:userId
+ @param block 该 block 会传一个字典，包含了注册的 URL 中对应的变量。
+ */
++ (void)registerURLPattern:(NSString *)URLPattern forObjectBlock:(LMQRouterObjectBlock)block;
+
+/**
+ 取消注册URLPattern
+
+ @param URLPattern URLPattern
+ */
 + (void)deregisterURLPattern:(NSString *)URLPattern;
 
+
+
+/**
+ *  打开此 URL，带上参数，同时当操作完成时，执行额外的代码
+ *
+ *  @param URL 例如 lmq://path/4
+ *  @param prame 附加参数
+ *  @param completion URL 处理完成后的 callback，完成跟具体的业务相关
+ */
 + (void)openURL:(NSString *)URL withPrame:(NSDictionary *)prame completion:(LMQRouterBlock)completion;
 
 
-+ (void)registerURLPattern:(NSString *)URLPattern forObjectBlock:(LMQRouterObjectBlock)block;
+
+
+/**
+ * 获取URL对应的返回值
+ *
+ *  @param URL 带 Scheme，如 lmq://userview
+ *  @param prame 参数
+ */
 
 + (id)objectFromURL:(NSString *)URL withPrame:(NSDictionary *)prame;
 
 
+/**
+ 拼接URL
+
+ @param pattern 注册时的原始URL
+ @param parameters 调用时的参数值传递
+ @return 返回完整调用URL
+ */
 + (NSString *)generateURLWithPattern:(NSString *)pattern parameters:(NSArray *)parameters;
 
 @end
--- LMQRouterDemo/ViewController.m
+++ LMQRouterDemo/ViewController.m
@@ -15,25 +15,120 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
+    //注册全局响应
+    [LMQRouter registerURLPattern:@"lmq://" forBlock:^(NSDictionary *result) {
+        NSLog(@"我是全局响应，没有对应注册的open都会到这里来");
+        if (result) {
+            NSLog(@"%@",result);
+            NSLog(@"%@\n",result[LMQRouterParameterURLKey]);
+            NSLog(@"%@\n",result[LMQRouterParameterPrameKey]);
+            LMQRouterBlock block = result[LMQRouterParameterCompletionBlockKey];
+            if (block) {
+                block(nil);
+            }
+        }
+    }];
+    
+    //在提供服务的模块中注册服务
     [LMQRouter registerURLPattern:@"lmq://hahaha" forBlock:^(NSDictionary *result) {
         NSLog(@"你好啊");
         if (result) {
             NSLog(@"%@",result);
             NSLog(@"%@\n",result[LMQRouterParameterURLKey]);
             NSLog(@"%@\n",result[LMQRouterParameterPrameKey]);
-            NSLog(@"%@\n",result[LMQRouterParameterCompletionBlockKey]);
+            LMQRouterBlock block = result[LMQRouterParameterCompletionBlockKey];
+            if (block) {
+                block(nil);
+            }
         }
     }];
     
+    //在其他模块调用服务的同时进行传参
     [LMQRouter openURL:@"lmq://hahaha" withPrame:@{@"ss":@"sss"} completion:^(NSDictionary *result) {
         NSLog(@"Hello World!");
         if (result) {
             NSLog(@"%@",result);
         }
     }];
+    
+    
+    //通过URL传参
+    [LMQRouter registerURLPattern:@"lmq://key/:userId" forBlock:^(NSDictionary *result) {
+        NSLog(@"你好啊");
+        if (result) {
+            NSLog(@"%@",result);
+            NSLog(@"%@\n",result[LMQRouterParameterURLKey]);
+            
+            
+            NSDictionary *prame = result[LMQRouterParameterPrameKey];
+            if (prame) {
+                NSLog(@"prame--%@\n",prame);
+                //模拟网络请求
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    
+                    LMQRouterBlock block = result[LMQRouterParameterCompletionBlockKey];
+                    if (block) {
+                        block(@{@"userId":prame[@"userId"],@"headPhoto":@"http:xxxx.jpg",@"nickName":@"doge"});
+                    }
+                });
+            }
+            
+        }
+    }];
+    
+    
+    //调用
+    [LMQRouter openURL:@"lmq://key/3" withPrame:nil completion:^(NSDictionary *result) {
+        NSLog(@"Hello World!");
+        if (result) {
+            NSLog(@"%@",result);
+        }
+    }];
+    
+    
+    //模块间使用方式
+#define URL @"lmq://orderDetail/:orderId"//一个全局服务的定义
+    //
+    [LMQRouter registerURLPattern:URL forBlock:^(NSDictionary *result) {
+        NSLog(@"开始查找订单喽");
+        if (result) {
+
+            NSDictionary *prame = result[LMQRouterParameterPrameKey];
+            if (prame) {
+                NSLog(@"prame--%@\n",prame);
+                //模拟网络请求
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    
+                    LMQRouterBlock block = result[LMQRouterParameterCompletionBlockKey];
+                    if (block) {
+                        block(@{@"OrderId":prame[@"orderId"],@"image":@"http:xxxx.jpg",@"price":@"200"});
+                    }
+                });
+            }
+            
+        }
+    }];
+    
+    //此调用可在其他模块之中
+    [LMQRouter openURL:[LMQRouter generateURLWithPattern:URL parameters:@[@3344]] withPrame:nil completion:^(NSDictionary *result) {
+        NSLog(@"拿到订单详细");
+        NSLog(@"%@",result);
+    }];
+    
+    
+    //注册URL对应的对象
+    [LMQRouter registerURLPattern:@"mgj://userView" forObjectBlock:^id(NSDictionary *routerParameters) {
+        UIView *userView = [[UIView alloc] init];
+        return userView;
+    }];
+    
+    //获取URL对应的对象
+    UIView *userView = [LMQRouter objectFromURL:@"mgj://userView" withPrame:nil];
+    NSLog(@"%@",userView.description);
 }
 
 
